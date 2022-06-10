980402c6bd9f7122f8ef095eb24e3206cbdbba65
JackyChang <jacky850509@gmail.com>
2022-06-10 11:34:43 +0800

    Init project

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"math/rand"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	F = "F"
+	R = "R"
+	L = "L"
+	T = "T"
+)
+
+var letterRunes = []rune("FRLT")
+
+func RandStringRunes(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
+
+func getPOST(ctx *gin.Context) {
+	j := make(map[string]interface{})
+	err := ctx.BindJSON(&j)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.String(http.StatusOK, RandStringRunes(1))
+}
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run(":8080")
+}
